632a6e402513bc41198b666825a5e9d3d492b6f4
Kyle Coburn <kylecoburn@gmail.com>
2020-12-04 10:20:11 -0700

    ♻️ check fields helpers

--- Pages/Day 4.xcplaygroundpage/Contents.swift
+++ Pages/Day 4.xcplaygroundpage/Contents.swift
@@ -1,28 +1,19 @@
 //: [Previous](@previous)
 
-let requiredFields = [
-	"byr",
-	"iyr",
-	"eyr",
-	"hgt",
-	"hcl",
-	"ecl",
-	"pid",
-//	"cid",
-]
+enum RequiredField: String, CaseIterable {
+	case byr, iyr, eyr, hgt, hcl, ecl, pid //, cid
+}
+
+func hasField(requiredField: RequiredField, passportFields: [String]) -> Bool {
+	return passportFields.first { $0.starts(with: requiredField.rawValue) } != nil
+}
+
+func hasAllRequiredFields(passportFields: [String]) -> Bool {
+	return RequiredField.allCases.first { !hasField(requiredField: $0, passportFields: passportFields) } == nil
+}
 
 let passportFieldsEntries = getPuzzleInput(separatedBy: "\n\n")
 	.map { $0.components(separatedBy: .whitespacesAndNewlines) }
-
-func hasAllRequiredFields(passportFields: [String]) -> Bool {
-	let missingFields = requiredFields.filter { requiredField in
-		passportFields.first { $0.starts(with: requiredField) } == nil
-	}
-	if !missingFields.isEmpty {
-		print(passportFields, missingFields)
-	}
-	return missingFields.isEmpty
-}
 
 let validPassports = passportFieldsEntries
 	.filter(hasAllRequiredFields(passportFields:))
