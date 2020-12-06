916f600c3366af6957538ed1f97df9f17f80742a
Kyle Coburn <kylecoburn@gmail.com>
2020-12-06 14:07:55 -0700

    ✨ day 6, part 2

--- Pages/Day 6.xcplaygroundpage/Contents.swift
+++ Pages/Day 6.xcplaygroundpage/Contents.swift
@@ -8,4 +8,16 @@ let questionsAnsweredByAnyoneInGroupCount: [Int] = individualsAnswersByGroup.map
 }
 print("Part 1:", questionsAnsweredByAnyoneInGroupCount.reduce(0, +))
 
+let questionsAnsweredByEveryoneInGroupCount: [Int] = individualsAnswersByGroup.map { group in
+	print("G", group.count)
+	var groupRemaining = group
+	let firstPersonAnswers = groupRemaining.removeFirst()
+	var answersForAllInGroup = Array(firstPersonAnswers)
+	groupRemaining.forEach { individualAnswers in
+		answersForAllInGroup.removeAll { !individualAnswers.contains($0) }
+	}
+	return answersForAllInGroup.count
+}
+print("Part 2:", questionsAnsweredByEveryoneInGroupCount.reduce(0, +))
+
 //: [Next](@next)
