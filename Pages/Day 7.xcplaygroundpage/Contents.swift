aa6d9e5936549021bf32eae34431367d52d938cf
Kyle Coburn <kylecoburn@gmail.com>
2020-12-07 15:01:57 -0700

    ♻️ track bags by parents and child counts

--- Pages/Day 7.xcplaygroundpage/Contents.swift
+++ Pages/Day 7.xcplaygroundpage/Contents.swift
@@ -1,29 +1,41 @@
 //: [Previous](@previous)
 
-var bagContainerNames: [String: [String]] = [:]
+var bagParentsChildrenByName: [String: ParentsChildren] = [:]
+
+typealias ParentsChildren = (parents: [String], children: [BagContained])
+
+struct BagContained {
+	let name: String
+	let count: Int
+}
+
+func getBagsContaining(name: String) -> [String] {
+	guard let parentsChildren = bagParentsChildrenByName[name] else {
+		return []
+	}
+	let parents = parentsChildren.parents
+	return parents + parents.flatMap(getBagsContaining)
+}
 
 getPuzzleInput().forEach { line in
 	let split = line.components(separatedBy: " contain ")
 	let parent = split.first!.split(separator: " ").dropLast().joined(separator: " ")
 	let children = split.last!.components(separatedBy: ", ")
+	if bagParentsChildrenByName[parent] == nil {
+		bagParentsChildrenByName[parent] = (parents: [], children: [])
+	}
 	children.forEach { numberAndNameLine in
 		var numberAndName = numberAndNameLine.split(separator: " ")
-		guard let _ = Int(numberAndName.removeFirst()) else {
+		guard let count = Int(numberAndName.removeFirst()) else {
 			return
 		}
-		let name = numberAndName.dropLast().joined(separator: " ")
-		if bagContainerNames[name] == nil {
-			bagContainerNames[name] = []
+		let child = numberAndName.dropLast().joined(separator: " ")
+		if bagParentsChildrenByName[child] == nil {
+			bagParentsChildrenByName[child] = (parents: [], children: [])
 		}
-		bagContainerNames[name]!.append(parent)
+		bagParentsChildrenByName[child]!.parents.append(parent)
+		bagParentsChildrenByName[parent]!.children.append(BagContained(name: child, count: count))
 	}
-}
-
-func getBagsContaining(name: String) -> [String] {
-	guard let containing = bagContainerNames[name] else {
-		return []
-	}
-	return containing + containing.flatMap(getBagsContaining)
 }
 
 do {
