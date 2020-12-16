dbe1b72f56c6db68962c01e07f63261415c14cbb
Kyle Coburn <kylecoburn@gmail.com>
2020-12-15 17:27:00 -0700

    ✨ day 10, part 2

--- Pages/Day 10.xcplaygroundpage/Contents.swift
+++ Pages/Day 10.xcplaygroundpage/Contents.swift
@@ -1,7 +1,8 @@
 //: [Previous](@previous)
 
-let input = loadPuzzleInput().compactMap(\.integerRepresentation)
+let adapterJoltCompatibility = 1...3
 
+let input: [Int] = loadPuzzleInput().compactMap(\.integerRepresentation)
 let orderedAdapters = input.sorted()
 
 do {
@@ -10,17 +11,27 @@ do {
 	var joltDifferences3 = 1
 	for adapterJolts in orderedAdapters {
 		let distanceBetweenAdapters = adapterJolts - previousAdapterJolts
-		if distanceBetweenAdapters > 3 {
+		if distanceBetweenAdapters > adapterJoltCompatibility.upperBound {
 			fatalError("Jolt gap too large: \(adapterJolts) \(previousAdapterJolts)")
 		}
 		previousAdapterJolts = adapterJolts
 		if distanceBetweenAdapters == 1 {
 			joltDifferences1 += 1
-		} else if distanceBetweenAdapters == 3 {
+		} else if distanceBetweenAdapters == adapterJoltCompatibility.upperBound {
 			joltDifferences3 += 1
 		}
 	}
 	print("Part 1:", joltDifferences1 * joltDifferences3)
+}
+
+do {
+	var countsByAdapter = [0: 1]
+	orderedAdapters.forEach { adapter in
+		countsByAdapter[adapter] = adapterJoltCompatibility.reduce(0) { accumulator, joltDistance in
+			accumulator + (countsByAdapter[adapter - joltDistance] ?? 0)
+		}
+	}
+	print("Part 2:", countsByAdapter[orderedAdapters.last!]!)
 }
 
 //: [Next](@next)
