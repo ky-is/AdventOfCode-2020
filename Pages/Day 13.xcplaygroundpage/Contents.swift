5f1a00d3b6f8eb5cdfed4e19502d58117e692e9c
Kyle Coburn <kylecoburn@gmail.com>
2020-12-16 17:02:26 -0700

    ✨ day 13, part 1

--- Pages/Day 13.xcplaygroundpage/Contents.swift
+++ Pages/Day 13.xcplaygroundpage/Contents.swift
@@ -1,5 +1,19 @@
 //: [Previous](@previous)
 
+let input = loadPuzzleInput()
 
+let timestamp = Int(input.first!)!
+let busIDs = input.last!
+	.split(separator: ",")
+	.compactMap(\.integerRepresentation)
+
+do {
+	let busTimestamps = busIDs
+		.map { id in id - (timestamp % id) }
+	let minMinsToWait = busTimestamps.min()!
+	let bestBusIndex = busTimestamps
+		.firstIndex(of: minMinsToWait)!
+	print("Part 1:", busIDs[bestBusIndex] * minMinsToWait)
+}
 
 //: [Next](@next)
