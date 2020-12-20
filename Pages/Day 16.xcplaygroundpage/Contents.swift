9160d92a9ead06e7266fe586650ee9b8276bd4cf
Kyle Coburn <kylecoburn@gmail.com>
2020-12-19 19:45:27 -0700

    ✨ day 16, part 1

--- Pages/Day 16.xcplaygroundpage/Contents.swift
+++ Pages/Day 16.xcplaygroundpage/Contents.swift
@@ -2,6 +2,30 @@
 
 import Foundation
 
-let input = loadPuzzleInput()
+let input = loadPuzzleInput(separatedBy: "\n\n")
+	.map { $0.components(separatedBy: .newlines) }
+
+let validators = input[0]
+	.map { $0.components(separatedBy: ": ").last! }
+	.flatMap { $0.components(separatedBy: " or ") }
+	.map { $0.split(separator: "-") }
+	.map { ClosedRange(uncheckedBounds: (Int($0[0])!, Int($0[1])!)) }
+
+do {
+	let invalidFields = input.last!
+		.dropFirst()
+		.map { $0.split(separator: ",") }
+		.compactMap { ticketFields -> [Int]? in
+			let invalidFields = ticketFields
+				.compactMap(\.integerRepresentation)
+				.filter { field in
+//					return true
+					validators.first { $0.contains(field) } == nil
+				}
+			return invalidFields.isEmpty ? nil : invalidFields
+		}
+		.flatMap { $0 }
+	print("Part 1:", invalidFields.reduce(0, +))
+}
 
 //: [Next](@next)
