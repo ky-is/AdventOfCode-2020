8a948fd91e25c192df6a8ce4e34f353fd03cac56
Kyle Coburn <kylecoburn@gmail.com>
2020-12-19 19:11:24 -0700

    ✨ day 15, part 1

--- Pages/Day 15.xcplaygroundpage/Contents.swift
+++ Pages/Day 15.xcplaygroundpage/Contents.swift
@@ -1,5 +1,25 @@
 //: [Previous](@previous)
 
 let input = loadPuzzleInput(separatedBy: ",")
+	.compactMap(\.integerRepresentation)
+
+do {
+	let goalIndex = 2020
+	var spokenNumbers = input
+	var currentNumber = spokenNumbers.last!
+	var previouslySpoken = spokenNumbers.dropLast()
+	for turnIndex in input.count..<goalIndex {
+		let previousNumber = currentNumber
+		if previouslySpoken.contains(currentNumber) {
+			let previousIndex = previouslySpoken.lastIndex(of: currentNumber)!
+			currentNumber = turnIndex - previousIndex - 1
+		} else {
+			currentNumber = 0
+		}
+		spokenNumbers.append(currentNumber)
+		previouslySpoken.append(previousNumber)
+	}
+	print("Part 1:", currentNumber)
+}
 
 //: [Next](@next)
